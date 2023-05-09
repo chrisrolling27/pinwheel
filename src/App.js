73714d24c289598a99b18784b1fae0534c73a09d
chris <chrisrolling27@gmail.com>
2023-05-09 14:40:07 -0700

    pinwheel backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import PinwheelModal from '@pinwheel/react-modal'
+import "./App.css";
+import PinwheelModal from "@pinwheel/react-modal";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      hi mom
-      <PinwheelModal linkToken={token} open={true} />
+        hi mom
+        {/* <PinwheelModal linkToken={token} open={true} /> */}
       </header>
     </div>
   );
--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const pinwheel = axios.create({
+  baseURL: "https://api.pinwheelapi.com/",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.PINWHEEL_API_SECRET}`,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
 app.listen(PORT, () => {
